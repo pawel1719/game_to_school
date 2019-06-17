14fd3ddfccf5fcca84afbd4ef2f501e78ee7bc31
pawel1719 <pawel.szostkiewicz@wp.pl>
2019-06-17 20:40:05 +0200

    Jedziemy!!!

--- hardLevelForGame.php
+++ hardLevelForGame.php
@@ -21,20 +21,6 @@ $game = playCity::getObject(Session::get('nick'));
 </HEAD>
 
 <BODY>
-<<<<<<< HEAD
-
-    <div class="card">
-        <h5 class="card-header">
-            <a href="level.php"><button class="btn btn-primary">POWRÓT</button></a>
-            <div class="text-center">GRA - zdadnij stolicę użytkowniku <b><?php echo $game->getNick(); ?></b></div>
-        </h5>
-        <div class="card-body">
-            <h6 class="card-title">
-                <?php
-
-                echo '<div class="col">Wynik: ' . ($game->getScore() * 10) . '/' . $game->getMaxScore() * 10;
-                echo '</div><div class="col">Liczba pozostałych pytań: ' . count(Session::get('questions'));
-=======
     <div class="head" style="width:800px; margin-top:35px; margin-left:auto; margin-right:auto;">
         <div class="float-left">
             <h2><b>GRA - Wisielec</b></h2>
@@ -44,16 +30,14 @@ $game = playCity::getObject(Session::get('nick'));
             <div>
                 <br><hr>    
             </div>
-        </div>
     </div>
-    <div class="card" style="width:800px; margin-top:35px; margin-left:auto; margin-right:auto;">
-    <div class="card-body">
-        <h6 class="card-title">
-            <?php 
+    <div class="card" style="width:800px; margin-top:35px; margin-left:auto; margin-right:auto; text-align: left; ">
+        <div class="card-body">
+            <h6 class="card-title">
+                <?php
 
-                echo '<div class="col">Wynik: '. ($game->getScore()*10) .'/'. $game->getMaxScore()*10; 
-                echo '</div><div class="col">Liczba pozostałych pytań: '. count(Session::get('questions'));
->>>>>>> 76b6ee5e4cdbc5be37dedbdae0748114dc35246a
+                echo '<div class="col">Wynik: ' . ($game->getScore() * 10) . '/' . $game->getMaxScore() * 10;
+                echo '</div><div class="col">Liczba pozostałych pytań: ' . count(Session::get('questions'));
                 echo '<hr/>';
 
                 //ACTION FOR FORM
@@ -88,32 +72,19 @@ $game = playCity::getObject(Session::get('nick'));
                     }
                 }
 
-<<<<<<< HEAD
                 ?>
         </div>
-=======
-            ?>
->>>>>>> 76b6ee5e4cdbc5be37dedbdae0748114dc35246a
         </h6>
         <h5 class="card-title"><?php echo $game->getQuestion()->question; ?></h5>
         <p class="card-text">
             <form method="POST">
-<<<<<<< HEAD
-                <input placeholder="Wpisz odpowiedź..." type="text" class="form-control" value="<?php echo $answer = (strlen($game->getQuestion()->answer) > 0) ? $game->getQuestion()->answer : ''; ?>" name="city" required /><br />
-=======
-                <input placeholder="Wpisz odpowiedź..." type="text" class="form-control" style="width: 200px;" value="<?php //echo $answer = (strlen($game->getQuestion()->answer)>0) ? $game->getQuestion()->answer : ''; ?>" name="city" required /><br/>
->>>>>>> 76b6ee5e4cdbc5be37dedbdae0748114dc35246a
+                <input placeholder="Wpisz odpowiedź..." type="text" style="width: 200px;" class="form-control" value="<?php //echo $answer = (strlen($game->getQuestion()->answer) > 0) ? $game->getQuestion()->answer : ''; ?>" name="city" required /><br />
                 <input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
                 <input type="hidden" name="score" value="<?php echo $score ?>" />
                 <input type="submit" value="Sprawdź" class="btn btn-primary" />
             </form>
         </p>
     </div>
-<<<<<<< HEAD
-=======
-    </div>
-    </div>
->>>>>>> 76b6ee5e4cdbc5be37dedbdae0748114dc35246a
 
     <footer></footer>
 
